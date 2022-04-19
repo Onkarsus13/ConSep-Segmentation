19021ab1d123a01bbd0a97151da9c4f2640c585a
onkarsus13 <onkarsus13@gmail.com>
2022-04-19 22:34:01 +0530

    updateed with clone

---
+++ Excumption_net_test/config.py
@@ -0,0 +1,19 @@
+class config:
+
+    epochs = 100
+    train_images = 'data/Train/images/*.png'
+    train_mask = 'data/Train/masks/*.png'
+    test_images = 'data/Test/images/*.png'
+    test_masks = 'data/Test/masks/*.png'
+    color_values = {
+                0: [0, 0, 0],
+                1: [255, 0, 0],
+                2: [255, 215, 0],
+                3: [0, 128, 0],
+                4: [0, 0, 255],
+                5: [139, 69, 19],
+                6: [47, 79, 79],
+                7: [25, 25, 112]
+                }
+    input_size = (256, 256, 3)
+    
---
+++ read.md
@@ -0,0 +1,9 @@
+To train the Excumption_net model just changes config setting according to your self,
+
+Dataset link: https://warwick.ac.uk/fac/sci/dcs/research/tia/data/hovernet/
+
+write this command in command promt 
+`python3 train.py`
+
+To infer:
+`python3 infer.py`
